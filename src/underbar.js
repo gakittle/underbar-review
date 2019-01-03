92639fe81ec3ce09f8c28173f981a12d7465ce82
gakittle <greg.kittleson@gmail.com>
2019-01-03 11:24:44 -0800

    Implement map through every

--- src/underbar.js
+++ src/underbar.js
@@ -114,6 +114,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var mapped = [];
+    _.each(collection, function(elem) {
+      mapped.push(iterator(elem));
+    });
+    return mapped;
   };
 
   /*
@@ -155,6 +160,16 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    if (accumulator === undefined) {
+      accumulator = collection[0];
+      collection = collection.slice(1);
+    }
+
+    _.each(collection, function(item) {
+      accumulator = iterator(accumulator, item);
+    });
+
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
@@ -173,6 +188,14 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    iterator = iterator || _.identity;
+    return _.reduce(collection, function(isTrue, item) {
+      if (isTrue) {
+        if (iterator(item)) {
+          return true;
+        } else {return false;}
+      } else {return false;}
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
